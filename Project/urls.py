11252663e7d2a3813f040db8e8954cd2a3217507
Fahim Arefin <fahimarefin83@gmail.com>
2023-12-21 01:34:52 +0600

    Done

--- Project/urls.py
+++ Project/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from project412 import views  # Make sure to import your views module
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('project412.urls')),
    # path('', views.index, name='index'),  # Add this line for the root URL
 ]
+
+if settings.DEBUG:
+  import debug_toolbar
+  urlpatterns +=path('__debug__/',include(debug_toolbar.urls)),
